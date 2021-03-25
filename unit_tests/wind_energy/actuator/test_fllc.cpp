73a2460e85b6aa84c10510a5e3bf05045f11da85
tonyinme <tonyinme@gmail.com>
2021-03-24 21:52:48 -0600

    Push to repo initial changes

---
+++ unit_tests/wind_energy/actuator/test_fllc.cpp
@@ -0,0 +1,9 @@
+#include "aw_test_utils/MeshTest.H"
+#include "test_act_utils.H"
+
+#include "amr-wind/wind_energy/actuator/turbine/fast/TurbineFast.H"
+#include "amr-wind/wind_energy/actuator/turbine/fast/turbine_fast_ops.H"
+#include "amr-wind/wind_energy/actuator/ActParser.H"
+#include "amr-wind/wind_energy/actuator/Actuator.H"
+
+
